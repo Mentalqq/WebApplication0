dfb2325b582499d74bf26b1a89e2b1cd9aa47a0e
Artem Koliadintsev <artem.koliadintsev@globallogic.com>
2022-04-19 16:03:11 +0300

    add UserRepository test

--- WebApplication1/Application/Options/DapperConnectionOptions.cs
+++ WebApplication1/Application/Options/DapperConnectionOptions.cs
@@ -6,7 +6,7 @@ namespace WebApplication1.Application.Options
     {
         public string SqlServerConnection { get; set; }
     }
-    public class DapperConnectionIOptions<T> : IOptions<T> where T : DapperConnectionOptions
+    public class ConnectionIOptions<T> : IOptions<T> where T : DapperConnectionOptions
     {
         public T Value { get; set; }
     }
